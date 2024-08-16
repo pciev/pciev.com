e11e004a0179f49dc939d2e055f6b61f3bacd881
pciev <coyne@lonecomputing.com>
2024-08-16 04:51:26 -0400

    Test js motr

---
+++ js/motr.js
@@ -0,0 +1,10 @@
+var motr = [
+    "Kikou!",
+    "Salut!",
+    "How much wood could a woodchuck chuck if a woodchuck could chuck wood",
+    "HAHA YES I'VE MADE OUT OF THE STRIP CLUB ALIVE , FUCK YOU STUPIDS -@HenstonVennic",
+    "come to my stable -@rllyhungryhorse",
+    "I'm going to Pranks you! -Jongulo Pranks"
+]
+var choice = Math.floor(Math.random() * textArray.length);
+document.getElementById("motr").innerHTML = textArray[choice];
