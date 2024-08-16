8eceddbfff152ddcbdf14bacee0dbed7de57a944
pciev <coyne@lonecomputing.com>
2024-08-16 05:52:20 -0400

    Test FORZATHON check

--- js/quote.js
+++ js/quote.js
@@ -1,10 +1,33 @@
+// Message Of The Refresh
 var motr = [
-    "Kikou!",
-    "Salut!",
-    "How much wood could a woodchuck chuck if a woodchuck could chuck wood",
+    "Kikou! -Chocolat Gelato",
+    "Salut! -Red Savarin",
     "HAHA YES I'VE MADE OUT OF THE STRIP CLUB ALIVE , FUCK YOU STUPIDS -@HenstonVennic",
     "come to my stable -@rllyhungryhorse",
-    "I'm going to Pranks you! -Jongulo Pranks"
+    "I'm going to Pranks you! -Jongulo Pranks",
+    "Squeak! -Red Cylinder",
+    "Squeak! -Elh Cylinder",
 ]
-var choice = Math.floor(Math.random() * motr.length);
-document.getElementById("motr").innerHTML = motr[choice];
+
+var min = getMinutes();
+forzathonCheck(min)
+
+// Default function
+function motr() {
+    var choice = Math.floor(Math.random() * motr.length);
+    document.getElementById("motr").innerHTML = motr[choice];
+}
+
+// #FORZATHON LIVE timer to run at 5 minutes before the next hour
+function forzathonCheck(min) {
+    if (min >= 55) {
+        document.getElementById("motr").innerHTML = "#FORZATHON LIVE - Event begins in" + " " + ". Get there now to take part!";
+    }
+}
+
+// Grab the current minute on page refresh
+function getMinutes() {
+    const date = new Date();
+    let minutes = date.getUTCMinutes();
+    return minutes;
+}
